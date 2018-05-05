0cbd1cfb9234b09b3b97a9c8a3e8facb342797d2
alaa_moustapha <alaamoustapha21@gmail.com>
2018-05-05 18:23:30 +0200

    header and footer,service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,30 @@
+//modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { HttpClientModule } from '@angular/common/http';
 
-
+//components
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+
+//services
+import {GetDataService} from './get-data.service';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    NgbModule.forRoot()
+    NgbModule.forRoot(),
+    HttpClientModule
   ],
-  providers: [],
+  providers: [GetDataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/get-data.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+
+@Injectable()
+export class GetDataService {
+
+  constructor(private http:HttpClient) { }
+  getDataFunc(path:string):Observable<any>{
+    return this.http.get(path);
+  }
+
+}
