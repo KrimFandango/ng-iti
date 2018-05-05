cda4a38bc3024a87784a7c434bcff20039c47a68
Alaa Said <alaasaid1699@gmail.com>
2018-05-05 18:12:07 +0200

    contact page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,33 @@
+//form
+import { FormsModule }   from '@angular/forms';
+//fontawesome
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+//services
+import { HttpClientModule } from '@angular/common/http';
+import{ DataService } from './data.service';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 
 import { AppComponent } from './app.component';
+import { ContactComponent } from './contact/contact.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
-    NgbModule.forRoot()
+    NgbModule.forRoot(),
+    FormsModule,
+    AngularFontAwesomeModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ DataService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/contact/contact.component.ts
@@ -0,0 +1,63 @@
+import { Component, OnInit } from '@angular/core';
+//form
+import { NgForm , NgModel } from '@angular/forms';
+//service
+import{ DataService } from'../data.service';
+// import {NgModel} from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-contact',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.scss']
+})
+export class ContactComponent implements OnInit {
+  //form variable
+  dataForm :object;
+  //services variable
+  contactData:Array<object>;
+  constructor(
+    private data : DataService
+  ) {
+    this.contactData=[];
+    this.getContactData();
+    this.dataForm={};
+   }
+
+  //services
+  getContactData():void{
+    let path : string =' ./assets/data.json';
+    this.data.getData(path).subscribe(
+      res => {
+        this.contactData=res;
+        // console.log(res);
+      },
+      err => {
+        console.log(err);
+      },
+      ()=>{}
+    );
+    
+  }
+  
+
+// form
+
+contact( data : NgForm ):void{
+  if(!data.valid)
+  {
+    console.log("error");
+  } 
+  else{
+    console.log(data);
+    console.log(this.dataForm);
+  }
+  
+
+}
+  
+  
+  
+  ngOnInit() {
+  }
+
+}
