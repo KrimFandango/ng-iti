4aa3b8509c3ba62229e20b4039e0304a4755ee1a
sahar E. Hassan <sahar.e.hassan2015@gmail.com>
2018-05-06 00:32:38 +0200

    Login component added.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-
+import { FormsModule } from '@angular/forms';
 // =================== bootstrap ======================
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 // ================= app components ====================
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+
 // ================== services =========================
 import { DataService } from './data.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     NgbModule.forRoot(),
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [DataService],
   bootstrap: [AppComponent]
