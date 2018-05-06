6cbbdbe95ef4c9c3220ef2f5c761a40c865266e9
omniayahia <omniayahia2411@gmail.com>
2018-05-06 11:30:35 +0200

    contact component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-
+import { FormsModule }   from '@angular/forms';
 // =================== bootstrap ======================
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 // ================= app components ====================
 import { AppComponent } from './app.component';
+import { ContactComponent } from './contact/contact.component';
+
 // ================== services =========================
 import { DataService } from './data.service';
 
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
     NgbModule.forRoot(),
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [DataService],
   bootstrap: [AppComponent]
