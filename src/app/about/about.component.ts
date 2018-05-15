bc129005be79eae77bb59662b12411c3615f54f2
madanader1994 <nadanader94@hotmail.com>
2018-05-15 21:14:15 +0200

    Finished About Us page and some css as well as modifying home page

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-about',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent implements OnInit {
-
-  constructor() { }
+ aboutData: Array<any>;
+  constructor(private q:DataService) {
+  this.getaboutData();
+}
 
   ngOnInit() {
   }
+  getaboutData(): void{
+  	let path:string='././assets/aboutData.json';
+  	this.q.getData(path).subscribe(
 
+  		res => {
+  			this.aboutData= res;
+  			console.log(res)
+  		},
+  		err => {
+  			console.log(err)
+  		}
+  		);
+}
 }
