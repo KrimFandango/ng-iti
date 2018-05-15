bb88c6ee48f13b3b65321ffa55171e0288751cc0
adelsadek <adelabodoma@gmail.com>
2018-05-15 15:26:06 +0200

    pull fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { NgbdCarouselBasic } from './header/carousel-basic';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NgbdCarouselBasic,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
