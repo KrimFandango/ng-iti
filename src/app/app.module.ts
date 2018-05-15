7aac8fed7346a2246b3fd5d83de65051348840bd
madanader1994 <nadanader94@hotmail.com>
2018-05-15 18:51:48 +0200

    Finished header and footer with routing to all pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule }   from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+
 // =================== bootstrap ======================
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
@@ -14,21 +16,38 @@ import { RegisterComponent } from './register/register.component';
 
 // ================== services =========================
 import { DataService } from './data.service';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
 
 
+const appRoutes: Routes = [
+  { path: '',      component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register',      component: RegisterComponent },
+  { path: 'contact',      component: ContactComponent },
+  { path: 'about',      component: AboutComponent }
+  
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     ContactComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    HeaderComponent,
+    HomeComponent,
+    FooterComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     NgbModule.forRoot(),
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [DataService],
   bootstrap: [AppComponent]
