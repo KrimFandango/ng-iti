51f98ba7e5aa421a4574512fe330b3fe83bf4ccb
marwa <marwafathallah85@gmail.com>
2018-05-15 13:15:52 +0200

    marwa task

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
-
+import { ContactusComponent } from './contactus/contactus.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactusComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
